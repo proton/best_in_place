d9f08cde229e76b8b271c87c0d01ddba1077beb0
Peter Savichev (proton) <psavichev@gmail.com>
2011-10-05 13:44:48 +0400

    fixed merge error in best_in_place.rb

--- lib/best_in_place.rb
+++ lib/best_in_place.rb
@@ -1,1 +1,9 @@
 module BestInPlace
+  require "best_in_place/helpers"
+
+  class Engine < ::Rails::Engine
+    initializer "setup for rails" do
+      ActionView::Base.send(:include, BestInPlace::Helpers)
+    end
+  end
+end
